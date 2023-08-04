a87aced65ba939a7a5f2a35d445e18c3ce717861
kyndalbowers <131105491+kyndalbowers@users.noreply.github.com>
2023-08-03 21:08:39 -0700

    generate and save functions

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const shapes = require('./lib/shapes');
+
 const prompts = [
     {
         type: "input",
@@ -28,3 +32,59 @@ const prompts = [
         message: "Please enter a color keyword OR hexadecimal number for the shape",
     },
 ]
+
+async function promptUser() {
+    const userInput = await inquirer.prompt(prompts);
+
+    let shapeInstance;
+    switch (userInput.shape) {
+        case 'circle':
+            shapeInstance = new shapes.Circle();
+        break;
+        case 'triangle':
+            shapeInstance = new shapes.Triangle();
+        break;
+        case 'square':
+            shapeInstance = new shapes.Square();
+        break;
+    default:
+        console.log('Invalid shape selected.');
+        return;
+    }
+
+    shapeInstance.setColor(userInput.shapeColor);
+
+    return {userInput, shapeInstance };
+
+    generateLogo();
+}
+
+async function generateLogo() {
+    const userInput = await promptUser();
+
+    const logoSvg = `
+    <svg width="300" height="200">
+        ${userInput.shapeInstance.render()}
+        <text x="50%" y="50%" text-anchor="middle" fill="${userInput.textColor}" font-size="50">${userInput.text}</text>
+    </svg>`;
+
+    return logoSvg;
+
+    saveLogoToFile();
+}
+
+async function saveLogoToFile(logoSvg, filename) {
+    try {
+    fs.writeFileSync(filename, logoSvg);
+    console.log(`Generated ${filename}`);
+    } catch (error) {
+    console.error('Error saving SVG file:', error);
+    }
+}
+
+// async function main() {
+    // const logoSvg = await generateLogo();
+    // saveLogoToFile(logoSvg, 'logo.svg');
+// }
+
+// main();
